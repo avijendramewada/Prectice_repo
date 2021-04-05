9782b237a448ba4dd68e65170bf817e1d6acd705
vijendra mewada <53970587+avijendramewada@users.noreply.github.com>
2021-04-05 18:47:52 +0530

    Add files via upload

---
+++ view_details.js
@@ -0,0 +1,18 @@
+function saveArrayData() {
+  var queryString = decodeURIComponent(window.location.search);
+  queryString = queryString.substring(1);
+  var storedData = queryString.split(",");
+  console.log('queries', storedData);
+  var list = document.getElementById('output');
+        const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${ storedData[0]}</td>
+      <td>${storedData[1]}</td>
+       <td>${storedData[2]}</td>
+        <td>${storedData[3]}</td>
+         <td>${storedData[4]}</td>
+          <td>${storedData[5]}</td>
+    `; 
+    list.appendChild(row);
+}
+  
