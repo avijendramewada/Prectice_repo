9b82a85a90b16bf6a1b8520a3de8c95a691ad43a
vijendra mewada <53970587+avijendramewada@users.noreply.github.com>
2021-04-06 13:30:47 +0530

    Add files via upload

---
+++ edit_form.js
@@ -0,0 +1,74 @@
+function saveArrayData() {
+    var queryString = decodeURIComponent(window.location.search);
+    queryString = queryString.substring(1);
+    var storedData = queryString.split(",");
+    console.log('queries', storedData);
+    document.getElementById("name").value = storedData[0];
+    document.getElementById("mail").value = storedData[1];
+    document.getElementById("phonenumber").value = storedData[2];
+    document.getElementById("age").value = storedData[3];
+    document.getElementById("gender").value = storedData[4];
+    document.getElementById("msg").value = storedData[5];
+//      var user_name = document.myform.user_name.saveArrayData[0];
+//   var user_email = document.myform.user_email.saveArrayData[1];
+//   var user_number = document.myform.user_number.value;
+//   var age = document.myform.age.value;
+//   var user_message = document.myform.user_message.value;
+//   var gender = document.myform.gender.value;
+
+}
+
+
+var userData = [];
+
+
+function validateform() {
+  //document.getElementById("myText").value = "Johnny Bravo";
+  var user_name = document.myform.user_name.value;
+  var user_email = document.myform.user_email.value;
+  var user_number = document.myform.user_number.value;
+  var age = document.myform.age.value;
+  var user_message = document.myform.user_message.value;
+  var gender = document.myform.gender.value;
+
+
+  // document.myform.user_name.value = "Hello World";
+  // var inputs = document.myform.input[type=checkbox]
+  // var arrData = [];
+  //  inputs.forEach(function(input){
+  //   arrData.push({ id: input.id, checked: input.checked });
+  // });
+  
+  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  
+  
+  if (user_name == null || user_name == "") {
+    alert("Name can't be blank");
+    return false;
+  }
+  else if (reg.test(user_email) == false) {
+    alert("Enter valid Email");
+    return false;
+  }
+  else if (isNaN(user_number) || user_number < 1) {
+    alert("Enter valid mobile number.");
+    return false;
+  }
+  else if (isNaN(age) || age < 1) {
+    alert("Enter valid age");
+    return false;
+  }
+      else if (gender == null || gender == "") {
+    alert("please selet your gender");
+    return false;
+  }
+    
+   else {
+      userData = [user_name, user_email,user_number, age, gender, user_message];
+      userData.pop(userData);
+      // var array = JSON.parse(localStorage.getItem('user_details') || '[]');
+      // array.push(user_details);
+      localStorage.setItem(user_email, JSON.stringify(userData));
+      //  localStorage.setItem('inputs', JSON.stringify(arrData));
+    }
+}
