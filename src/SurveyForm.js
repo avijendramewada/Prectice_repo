32680bcf8af47f0cfefc41e94da883fea2459fbf
vijendra mewada <53970587+avijendramewada@users.noreply.github.com>
2021-04-08 16:28:11 +0530

    Update SurveyForm.js

--- src/SurveyForm.js
+++ src/SurveyForm.js
@@ -1,31 +1,30 @@
 import React,{useState} from 'react';
 import './Form.css';
-import { Link, Redirect } from 'react-router-dom';
 import { useHistory } from "react-router-dom";
-
 function SurveyForm(){
   const history = useHistory();
-  let [username, setName] = useState('');
-  let [email, setEmail] = useState('');
-  let [usernumber, setNumber] = useState('');
-  let [userage, setAge] = useState('');
-  let [gender, setGender] = useState('Male');
-  let [usermsg, setMsg] = useState('');
+  const [username, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [usernumber, setNumber] = useState('');
+  const [userage, setAge] = useState('');
+  const [gender, setGender] = useState('Male');
+  const [usermsg, setMsg] = useState('');
   //  const submitForm = e => {
   //   e.preventDefault();
   //   console.log("hello");
   //   this.props.history.push('/thank-you'); // <--- The page you want to redirect your user to.
   // }
-  function clearFeild() {
-    username = "";
-    email = "";
-    usernumber = "";
-    userage = "";
-    gender = "";
-    usermsg = "";
-  }
+  // function clearFeild() {
+  //   username = "";
+  //   email = "";
+  //   usernumber = "";
+  //   userage = "";
+  //   gender = "";
+  //   usermsg = "";
+  // }
   const Submit = e =>{
     e.preventDefault();
+
     const reg= /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
     if (username === null || username ==="") {
       alert("Name can't be blank");
@@ -35,7 +34,7 @@ function SurveyForm(){
       alert("Enter valid Email");
       return false;
     }
-    else if (isNaN(usernumber) || usernumber < 1|| usernumber.length === 10) {
+    else if (isNaN(usernumber) || usernumber < 1 || (usernumber.length)<10) {
       alert("Enter valid mobile number.");
       return false;
     }
@@ -61,34 +60,39 @@ function SurveyForm(){
     }
     return (
       <div className="container">
-      <div>
-        <form>
-          <h1>Survey form</h1>
-          <h5>Username</h5>
-          <input type="text" value={username} onChange={e=>setName(e.target.value) } placeholder="Enter name here"/>
-          <h5>Email</h5>
-          <input type="text" value={email} onChange={e=>setEmail(e.target.value) } placeholder="Enter email here"/>
-          <h5>Number</h5>
-          <input name="user_number" value={usernumber} onChange={e=>setNumber(e.target.value) } placeholder="Enter 10 digit number"/>
-          <h5>Age</h5>
-          <input type="number" value={userage} onChange={e=>setAge(e.target.value) } placeholder="Enter age here"/>
-          <div>
-            <h5>Gender</h5>
-            <input type="radio" name="gender" defaultChecked={gender==='Male' } value="Male" onChange={()=>setGender('Male')}/> Male
-            <input type="radio" name="gender" defaultChecked={gender==='Female' } value="Female" onChange={()=>setGender('Female')}/>Female
-          </div>
-          <div>
-            <label htmlFor="msg"></label>
-            <p>Any comment or suggestions?</p>
-            <textarea type="text" value={usermsg} onChange={e=>setMsg(e.target.value) } rows="4" cols="50" placeholder="Enter Text Here"></textarea>
-          </div>
-          <div id="submitbutton">
-            <button type="submit" id="submit" onClick={Submit}>Submit</button>
-          </div>
-        </form>
-      </div>
-    </div>
-    
+<form>
+               <h1 >Survey form</h1>
+          
+               <label>UserName
+               <input type="text" value ={username} onChange ={e => setName(e.target.value) } placeholder="Enter name here"/>
+               </label>
+                <label>Email
+                <input type="text"value ={email} onChange ={e => setEmail(e.target.value) } placeholder="Enter email here"/>  
+                </label>
+                <label>Mobile Number
+                <input type="nuber" name="user_number" value ={usernumber} onChange ={e => setNumber(e.target.value) } placeholder="Enter 10 digit number"/>
+                </label>
+                <label>Age
+                <input type="number" value ={userage} onChange ={e => setAge(e.target.value) } placeholder="Enter age here"/>  
+                </label>
+                {/* onChange={this.setGender.bind(this)} */}
+                <label>Gender<br/>
+                <input type="radio" name="gender" checked={gender === 'Male'} value="Male" onClick={() => setGender('Male')}/> Male<br/>
+               <input type="radio"  name="gender"checked={gender === 'Female'} value="Female" onClick={() => setGender('Female')}/>Female
+                  </label><br/>
+                <label  for="msg">
+                Any comment or suggestions?
+                <textarea type="text" value ={usermsg} onChange ={e => setMsg(e.target.value) } rows="4" cols="50" placeholder="Enter Text Here"></textarea>  
+                </label>
+              
+
+            
+                <div id="submitbutton">
+                <button type="submit" id="submit"  onClick ={Submit}>Submit</button>  
+            </div>
+
+</form>
+</div>    
     )
 }
 
